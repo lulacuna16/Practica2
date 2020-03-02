81c1d437b8f912ef00665ab747c652195d6168b1
Luis Cuevas <lulacuna16@gmail.com>
2020-03-01 22:35:29 -0600

    Version 4. Función en diferentes máquinas

--- main.py
+++ main.py
@@ -199,11 +199,11 @@ def servirPorSiempre(TCPServerSocket, listaconexiones):
     except Exception as e:
         print(e)
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+HOST = "192.168.1.64"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 buffer_size = 1024
 listaConexiones = []
-numConn=int(input("Ingrese Númerode conexiones a aceptar: "))
+numConn=int(input("Ingrese Número de conexiones a aceptar: "))
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCPServerSocket:
     TCPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     TCPServerSocket.bind((HOST, PORT))
